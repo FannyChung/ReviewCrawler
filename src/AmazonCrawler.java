3ee7341d7d21627ba862bb3003b98d63f706c5d7
钟芳 <zhongfang1993@163.com>
2015-04-11 16:09:48 +0800

    评论页去重&加代理
    代理效果不好，加大retry次数仍然没有什么效果

--- src/AmazonCrawler.java
+++ src/AmazonCrawler.java
@@ -1,4 +1,3 @@
-
 //package cn.edu.hfut.dmic.webcollector.example;
 
 import java.io.BufferedWriter;
@@ -8,6 +7,7 @@ import java.io.IOException;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Vector;
 
@@ -17,142 +17,171 @@ import cn.edu.hfut.dmic.webcollector.model.Page;
 import cn.edu.hfut.dmic.webcollector.net.Proxys;
 import cn.edu.hfut.dmic.webcollector.util.RegexRule;
 
-import org.apache.commons.lang3.ObjectUtils.Null;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 public class AmazonCrawler extends DeepCrawler {
-    RegexRule regexRule = new RegexRule();
-	BufferedWriter bufferWritter;
+	private RegexRule regexRule = new RegexRule();
+	private BufferedWriter bufferWritter;
 
-    JdbcTemplate jdbcTemplate = null;
-    private static HashSet<String> allLinksHashSet=new HashSet<String>();
-    Vector<Review> reviews=new Vector<Review>();
-    /*
-     * 构造函数
-     */
-    public AmazonCrawler(String crawlPath) {
-        super(crawlPath);
-        regexRule.addRule("http://www.amazon.cn/product-reviews/.*");
-    }
+	private ArrayList<String> allLinksHashSet = new ArrayList<String>();
+	private Vector<Review> reviews = new Vector<Review>();
+	private int SLEEP_TIME=10000;
 
-    public Links visitAndGetNextLinks(Page page) {
-        Document doc = page.getDoc();
-        String title = doc.title();
-        Elements ele1=doc.select("div[style=margin-left:0.5em;]");//评论相关信息的集合
-        System.out.println("URL:" + page.getUrl() + "  标题:" + title+"    "+page.getHtml());
-        try {
-			Thread.sleep(10000);
+	/*
+	 * 构造函数
+	 */
+	public AmazonCrawler(String crawlPath) {
+		super(crawlPath);
+		regexRule.addRule("http://www.amazon.cn/product-reviews/.*");
+	}
+
+	public Links visitAndGetNextLinks(Page page) {
+		String reviewReg = "div[style=margin-left:0.5em;]";
+		String textReg = ".reviewText";
+		String levelReg = "span[class~=swSprite s_star_.*]";
+		String titleReg = "b";
+		String timeReg = "nobr";
+		String userReg = "span[style = font-weight: bold;]";
+		String dateFormat = "yyyy年MM月dd日";
+
+		Document doc = page.getDoc();
+		String title = doc.title();
+		if (title.matches("Amazon CAPTCHA")) {
+			System.out
+					.println("访问过于频繁，被拒绝了！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！");
+			return null;
+		}
+		Elements ele1 = doc.select(reviewReg);// 评论相关信息的集合
+		System.out.println("URL:" + page.getUrl() + " \n标题:" + title);
+		try {
+			Thread.sleep(SLEEP_TIME);
 		} catch (InterruptedException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-        if (ele1.isEmpty()) {
-			System.out.println("没找到评论信息对应的元素！------------------------------------");
+		if (ele1.isEmpty()) {
+			System.out
+					.println("没找到评论信息对应的元素！------------------------------------");
 		} else {
-	        for (Element element : ele1) {//每个元素有评论、作者、时间等
-	            Review review = new Review();
-	            Elements textElements=element.select(".reviewText");//获取评论文本
-	    		String cString=textElements.get(0).text();
-	    		System.out.println(cString);
-	    		review.setText(cString);
-	    		
-	    		Elements levelElements=element.select("span[class~=swSprite s_star_.*]");//获取星级信息
-	    		cString=levelElements.get(0).text();
-	    		int level=cString.charAt(2)-'0';
-	    		System.out.println(level);
-	    		review.setLevel(level);
-	    		
-	    		Elements titleElements=element.select("span[style=vertical-align:middle;]");//获取评论的标题
-	    		cString=titleElements.select("b").text();
-	    		System.out.println(cString);
-	    		review.setReTitle(cString);
-	    		cString=titleElements.select("nobr").text();						//获取日期
-	    		System.out.println(cString);
-	    		SimpleDateFormat s = new SimpleDateFormat("yyyy年MM月dd日");
-	    		Date d=null; 
-	    		try {
+			for (Element element : ele1) {// 每个元素有评论、作者、时间等
+				Review review = new Review();
+				Elements textElements = element.select(textReg);// 获取评论文本
+				String cString = textElements.get(0).text();
+//				System.out.println(cString);
+				review.setText(cString);
+
+				Elements levelElements = element.select(levelReg);// 获取星级信息
+				cString = levelElements.get(0).text();
+				/*
+				 * TODO
+				 */
+				int level = cString.charAt(2) - '0';
+//				System.out.println(level);
+				review.setLevel(level);
+
+				Elements titleEle = element.select(titleReg);
+				cString = titleEle.get(0).text(); // 标题
+//				System.out.println(cString);
+				review.setReTitle(cString);
+
+				Elements timeEle = element.select(timeReg);
+				cString = timeEle.get(0).text(); // 获取日期
+//				System.out.println(cString);
+				SimpleDateFormat s = new SimpleDateFormat(dateFormat);
+				Date d = null;
+				try {
 					long t = s.parse(cString).getTime();
-		    		 d=new Date(t);
+					d = new Date(t);
 				} catch (ParseException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-	    		review.setTime(d);
-	    		
-	    		Elements userElements=element.select("span[style = font-weight: bold;]");//获取评论者名称
-	    		cString=userElements.get(0).text();
-	    		System.out.println(cString);
-	    		review.setUserName(cString);
-	    		
-	    		reviews.add(review);
-			}
-	        System.out.println("处理完评论信息============================================================");
-	        printText(reviews);
-		}
-        
-//        Elements as = doc.select("span.paging>*").select("a[href]");
-//    	String href=as.last().attr("abs:href");//下一页
-        Links nextLinks = new Links();
-//        nextLinks.addAllFromDocument(doc, regexRule);
-//        nextLinks.addAllFromDocument(doc, "span.paging>*");// 
-        Elements as = doc.select("span.paging>*").select("a[href]");
-        for (Element a : as) {
-            String href = a.attr("abs:href");
-        	System.out.println("Amazon获取-------------------\n"+href);
-            if(allLinksHashSet.add(href))
-            	nextLinks.add(href);
-        }
-        
-        return nextLinks;
-    }
+				review.setTime(d);
 
-    public void addToLinks(String link) {
+				Elements userElements = element.select(userReg);// 获取评论者名称
+				cString = userElements.get(0).text();
+//				System.out.println(cString);
+				review.setUserName(cString);
+
+				reviews.add(review);
+			}
+			System.out
+					.println("处理完评论信息============================================================");
+		}
+
+		Links nextLinks = new Links();
+		Elements as = doc.select("span.paging>*").select("a[href]");
+		if (as.isEmpty()) {
+			System.out
+					.println("没找到其他评论页面的超链接！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！");
+		}else {
+			String href = as.last().attr("abs:href");
+			System.out.println("Amazon获取-------------------\n" + href);
+			if (href.matches(".*link_next.*")) {
+				nextLinks.add(href);
+				allLinksHashSet.add(href);
+			}
+		}
+		return nextLinks;
+	}
+
+	public void addToLinks(String link) {
 		allLinksHashSet.add(link);
 	}
-    public void printText(Vector<Review> reviews) {
-    	File file = new File("out");
+
+	public void printText(Vector<Review> reviews) {
+		File file = new File("out.txt");
 		if (!file.exists()) {
 			try {
 				file.createNewFile();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-        FileWriter fileWritter = null;
+		FileWriter fileWritter = null;
 		try {
-			fileWritter = new FileWriter(file.getName(),true);
+			fileWritter = new FileWriter(file.getName(), true);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-         bufferWritter = new BufferedWriter(fileWritter);
-         
-         try {
-        	 for (Review review : reviews) {
-        		 bufferWritter.write(review.getLevel()+review.getText()+'\t'+review.getReTitle()+'\t'+review.getTime()+'\t'+review.getUserName());
-     			bufferWritter.newLine();
+		bufferWritter = new BufferedWriter(fileWritter);
+
+		try {
+			for (Review review : reviews) {
+				bufferWritter.write(review.getLevel() + review.getText() + '\t'
+						+ review.getReTitle() + '\t' + review.getTime() + '\t'
+						+ review.getUserName());
+				bufferWritter.newLine();
 			}
 			bufferWritter.newLine();
-			 bufferWritter.close();
+			bufferWritter.write("总数目："+reviews.size());
+			bufferWritter.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-         
-	}
-    public static void main(String[] args) throws Exception {
-        AmazonCrawler crawler = new AmazonCrawler("/home/hu/data/az");
-        crawler.setThreads(50);
-        crawler.addSeed("http://www.amazon.cn/product-reviews/B00OB5T26S/ref=cm_cr_dp_see_all_btm?ie=UTF8&showViewpoints=1&sortBy=bySubmissionDateDescending");
-        Proxys proxys = new Proxys();
-        crawler.setProxys(proxys);
 
-        /*设置是否断点爬取*/
-        crawler.setResumable(false);
-        crawler.start(1);
-    }
+	}
+
+	public void printAllLinks() {
+		printText(reviews);
+		for (String string : allLinksHashSet) {
+			System.out.println(string);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		AmazonCrawler crawler = new AmazonCrawler("/home/hu/data/az");
+		crawler.setThreads(1);
+		crawler.addSeed("http://www.amazon.cn/product-reviews/B00OB5T26S/ref=cm_cr_dp_see_all_btm?ie=UTF8&showViewpoints=1&sortBy=bySubmissionDateDescending");
+		crawler.addToLinks("http://www.amazon.cn/product-reviews/B00OB5T26S/ref=cm_cr_dp_see_all_btm?ie=UTF8&showViewpoints=1&sortBy=bySubmissionDateDescending");
+		Proxys proxys = new Proxys();
+		crawler.setProxys(proxys);
+
+		/* 设置是否断点爬取 */
+		crawler.setResumable(false);
+		crawler.start(5);
+		crawler.printAllLinks();
+	}
 }
--- src/ItemRevDemo.java
+++ src/ItemRevDemo.java
@@ -1,11 +1,8 @@
-
-import java.util.Vector;
+import java.io.IOException;
 import java.util.regex.Pattern;
 
-
-
-
-
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
@@ -13,82 +10,110 @@ import cn.edu.hfut.dmic.webcollector.crawler.DeepCrawler;
 import cn.edu.hfut.dmic.webcollector.model.Links;
 import cn.edu.hfut.dmic.webcollector.model.Page;
 import cn.edu.hfut.dmic.webcollector.net.Proxys;
-import cn.edu.hfut.dmic.webcollector.util.RegexRule;
 
-    public class ItemRevDemo extends DeepCrawler{  
-        RegexRule regexRule = new RegexRule();
-        Vector<Review> reviews=new Vector<Review>();
-    	AmazonCrawler crawler1=new AmazonCrawler("/home/hu/data/az");
-    	Links reviewLinks=new Links();
+public class ItemRevDemo extends DeepCrawler {
+	private AmazonCrawler crawler1 = new AmazonCrawler("/home/hu/data/az");
+	private int MAXNUM;
+	
+	private final int SLEEP_TIME=10000;
 
-        public ItemRevDemo(String crawlPath) {
-			super(crawlPath);
+	public ItemRevDemo(String crawlPath) {
+		super(crawlPath);
+	}
+
+	public Links visitAndGetNextLinks(Page page) {
+		Links nextLinks = new Links();
+		org.jsoup.nodes.Document doc = page.getDoc();
+		String url = page.getUrl();
+		System.out.println("URL:\t" + url + "\n标题:\t" + doc.title() + "\n");
+		if (doc.title().matches("Amazon CAPTCHA")) {
+			System.out
+					.println("访问过于频繁，被拒绝了！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！");
+			return null;
 		}
-		public Links visitAndGetNextLinks(Page page) {
-	        Links nextLinks = new Links();
-	        org.jsoup.nodes.Document doc=page.getDoc();
-	        String url=page.getUrl();
-	        System.out.println("URL:\t" + url + "\n标题:\t" + doc.title());
-	        
-	        if(Pattern.matches("^http://www.amazon.cn/product-reviews/.*", url)){	//如果是评论页面，加入”下一页“指向的链接
-	        	System.out.println("加入Amazon的种子============================================");
-	        	crawler1.addSeed(url);
-	        	crawler1.addToLinks(url);
-//	        	Elements as = doc.select("span.paging>*").select("a[href]");
-//	        	String href=as.last().attr("abs:href");//下一页
-//	        	nextLinks.add(href);
-	        	//如果是第一页，获取最大页数，并加入nextLinks
-//	        	
-//	        	Elements ele2=doc.select("span.page>a");
-//	        	String numString=ele2.last().text();
-//	        	int maxNum=Integer.parseInt(numString);
-//	        	
-//	        	for (int i = 2; i < maxNum; i++) {
-//					nextLinks.add("");
-//				}
-	        }else if (Pattern.matches("^http://www.amazon.cn/.*/dp/.*", url)) {	//如果是商品页面，加入”查看所有评论“对应的链接
-//		        nextLinks.addAllFromDocument(page.getDoc(), "a[class=a-link-emphasis a-text-bold]");
-	        	Elements as = doc.select("a[class=a-link-emphasis a-text-bold]");
-//	        	Elements as = doc.select("div[id=revF]>div>a");
-	        	if (as.isEmpty()) {
-					System.out.println("没找到评论网页！！！！！！！！！！！！！！！！！！！！！！！！！！！！！");
-				} else {
-		        	String hrefString=as.get(0).attr("abs:href");
-		        	System.out.println("获取-------------------\n"+hrefString);
-		        	nextLinks.add(hrefString);
-				}
+		try {
+			Thread.sleep(SLEEP_TIME);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		if (Pattern.matches("^http://www.amazon.cn/product-reviews/.*", url)) { // 如果是评论页面，加入”下一页“指向的链接
+			System.out
+					.println("加入Amazon的种子============================================");
+			crawler1.addSeed(url);
+			crawler1.addToLinks(url);
+			
+			//获取最大页数，以设置评论爬虫的深度
+			Elements as = doc.select("span.paging>*");
+			if(as.isEmpty()){
+				System.out.println("没有其他评论页~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~只有一页评论");
+				return null;
 			}
-	        try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			String numString=as.get(as.size()-2).text();
+			MAXNUM=Integer.parseInt(numString);
+			System.out.println("获取到页数："+MAXNUM+'\n');
+		} else if (Pattern.matches("^http://www.amazon.cn/.*/dp/.*", url)) { // 如果是商品页面，加入”查看所有评论“对应的链接
+			Elements as = doc.select("a[class=a-link-emphasis a-text-bold]");
+			if (as.isEmpty()) {
+				System.out.println("没找到评论网页！！！！！！！！！！！！！！！！！！！！！！！！！！！！！");
+			} else {
+				String hrefString = as.get(0).attr("abs:href");
+				System.out.println("获取评论首页-------------------\n" + hrefString);
+				nextLinks.add(hrefString);
 			}
-	        return nextLinks;
-		}  
+		}
+		return nextLinks;
+	}
+
+	public void startRev() {
+		crawler1.setThreads(1);
+		/* 设置是否断点爬取 */
+		crawler1.setResumable(false);
+		try {
+			crawler1.start(MAXNUM);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		crawler1.printAllLinks();
+	}
+
+	/*
+	 * 设置代理
+	 */
+	public void setPro() {
+		String dlWebUrl = "http://www.kuaidaili.com/free/inha/6";
+		Document doc = null;
+		try {
+			doc = Jsoup.connect(dlWebUrl).timeout(5000).get();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Element listDiv = doc.getElementById("list");  
+        Elements trs = listDiv.select("table tbody tr");
+		Proxys proxys = new Proxys();
+		for (final Element tr : trs) {  
+            String ip = tr.child(0).text();  
+            int port = Integer.parseInt(tr.child(1).text()); 
+            System.out.println("代理："+ip+"\nport: "+port);
+            proxys.add(ip, port);
+        }  
+		crawler1.setProxys(proxys);
+		this.setProxys(proxys);
 		
-		public void startRev() {
-			   Proxys proxys = new Proxys();
-	            crawler1.setProxys(proxys);
-	        	crawler1.setThreads(50);
-	            /*设置是否断点爬取*/
-	            crawler1.setResumable(false);
-	            try {
-					crawler1.start(5);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-		}
-        /*启动爬虫*/  
-        public static void main(String[] args) throws Exception{    
-            ItemRevDemo crawler=new ItemRevDemo("/home/hu/data/az123");  
-            crawler.setThreads(50);
-            crawler.addSeed("http://www.amazon.cn/SAMSUNG-%E4%B8%89%E6%98%9F-E1200R-GSM%E6%89%8B%E6%9C%BA/dp/B00KW0QFGG/ref=sr_1_1?ie=UTF8&qid=1428679002&sr=8-1&keywords=%E6%89%8B%E6%9C%BA");
-            Proxys proxys = new Proxys();
-            crawler.setProxys(proxys);
-            /*设置是否断点爬取*/
-            crawler.setResumable(false);
-            crawler.start(2);
-            crawler.startRev();
-        }
-    }  
+		this.setRetry(10);
+		crawler1.setRetry(10);
+	}
+	/* 启动爬虫 */
+	public static void main(String[] args) throws Exception {
+		ItemRevDemo crawler = new ItemRevDemo("/home/hu/data/az123");
+		crawler.setThreads(1);
+//		crawler.addSeed("http://www.amazon.cn/HUAWEI-%E5%8D%8E%E4%B8%BA-%E8%8D%A3%E8%80%80-3X%E7%95%85%E7%8E%A9%E7%89%88-G750-T01%E7%A7%BB%E5%8A%A83G%E6%89%8B%E6%9C%BA-%E5%8F%8C%E5%8D%A1%E5%8F%8C%E5%BE%85-%E7%9C%9F8%E6%A0%B8%E5%A4%84%E7%90%86%E5%99%A8-2GB-RAM-8GB-ROM-1300%E4%B8%87-500%E4%B8%87%E5%83%8F%E7%B4%A0/dp/B00JO9DCQM/ref=sr_1_3?ie=UTF8&qid=1428729973&sr=8-3&keywords=%E6%89%8B%E6%9C%BA");
+		crawler.addSeed("http://www.amazon.cn/%E6%9C%97%E6%A0%BC%E5%A4%A9%E5%90%AFL80%E6%89%8B%E6%9C%BA-%E5%85%AB%E6%A0%B84G%E6%89%8B%E6%9C%BA-4G%E6%99%BA%E8%83%BD%E6%89%8B%E6%9C%BA-1600%E4%B8%87%E9%AB%98%E6%B8%85%E5%83%8F%E7%B4%A0-%E7%A7%BB%E5%8A%A84G-%E5%8F%8C%E5%8D%A1%E5%8F%8C%E5%BE%852G-RAM-16G-ROM-%E5%A4%A7%E5%86%85%E5%AD%98%E6%99%BA%E8%83%BD%E6%89%8B%E6%9C%BA-5-5%E8%8B%B1%E5%AF%B8%E5%A4%A7%E5%B1%8F-%E5%AE%89%E5%8D%934-3%E7%B3%BB%E7%BB%9F/dp/B00RMUGQ18/ref=sr_1_302?s=wireless&ie=UTF8&qid=1428734065&sr=1-302&keywords=%E6%89%8B%E6%9C%BA");
+		crawler.setPro();
+		/* 设置是否断点爬取 */
+		crawler.setResumable(false);
+		crawler.start(2);
+		crawler.startRev();
+	}
+}
