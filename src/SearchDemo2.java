6c1b8838a11afec35272a8ebbeb5648d218e256d
钟芳 <zhongfang1993@163.com>
2015-04-11 17:47:28 +0800

    成功获取商品url列表
    利用Selenium模拟

--- src/SearchDemo2.java
+++ src/SearchDemo2.java
@@ -1,16 +1,15 @@
+import java.util.HashSet;
+import java.util.List;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class SearchDemo2 {
 
-	public void search(String homepageString, String searchString) {
+	public String search(String homepageString, String searchString) {
 		String inputReg="input[type=text]";
-		
 		HtmlUnitDriver driver = new HtmlUnitDriver();
 		driver.get(homepageString);
 		System.out.println("Page title is:" + driver.getTitle()+" url: "+driver.getCurrentUrl());
@@ -19,12 +18,32 @@ public class SearchDemo2 {
 		// 输入搜索关键字
 		element.sendKeys(searchString);
 		element.submit();
-		System.out.println("Page title is:" + driver.getTitle()+" url: "+driver.getCurrentUrl());
-		driver.quit();
+		String curUrl=driver.getCurrentUrl();
+		System.out.println("Page title is:" + driver.getTitle()+" url: "+curUrl);
+		driver.close();
+		return curUrl;
+	}
+	
+	public HashSet<ProductUrl> getProductPage(String searchUrl) {
+		HashSet<ProductUrl> products=new HashSet<ProductUrl>();
+		HtmlUnitDriver driver = new HtmlUnitDriver();
+		driver.get(searchUrl);
+		List<WebElement> link=driver.findElements(By.cssSelector("[href]"));
+		for (WebElement webElement : link) {
+			String href=webElement.getAttribute("href");
+			if(href.matches("http://www.amazon.cn/.*/dp/.*")&&products.add(new ProductUrl(href))){
+				System.out.println(webElement.getText()+'\t'+href);
+			}
+		}
+		System.out.println("over=================================================================");
+		System.out.println(products.size());
+		driver.close();
+		return products;
 	}
 
 	public static void main(String[] args) {
 		SearchDemo2 search = new SearchDemo2();
-		search.search("http://www.amazon.cn/ref=nav_logo", "kindle");
+		String s=search.search("http://www.amazon.cn/ref=nav_logo", "手机");
+		search.getProductPage(s);
 	}
 }
